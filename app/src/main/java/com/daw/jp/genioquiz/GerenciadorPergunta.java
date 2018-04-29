406213f3b7e6215918e80e14c94f6b34964aa7b0
punisher077 <arthur.mts@gmail.com>
2018-04-29 11:32:47 -0300

    Initial commit PERGUNTAS SOBRE CDZ E SAO COM BOOLLEAN

--- app/src/main/java/com/daw/jp/genioquiz/GerenciadorPergunta.java
+++ app/src/main/java/com/daw/jp/genioquiz/GerenciadorPergunta.java
@@ -72,6 +72,87 @@ public class GerenciadorPergunta {
             }
         };
         perguntas.add(new Pergunta("(Boku No Hero) Quais individualidades o Nomu que enfrenta All Might possui?", "Teletransposte", "Regeneração", "Absorvição de impacto", "Gelo e Fogo", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(true);
+                add(false);
+                add(false);
+                add(true);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros do Zodiaco) Qual desses são Cavaleiros de Ouro?", "Shaka", "Shun", "Orpheu", "Aldebaran", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(false);
+                add(false);
+                add(true);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros do Zodiaco) Qual dos Cavaleiros de Bronze era a reencarnação do deus Hades na Saga do Inferno?", "Seiya", "Ikki", "Shion", "Shun", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(false);
+                add(true);
+                add(true);
+            }
+        };
+        perguntas.add(new Pergunta("(Caveleiros do Zodiaco) Na mangá Episodio G que conta a historia dos Caveleiros de Ouro, quais desses golpes pertencem a Aiolia de Leão?", "Colera do Dragãpo", "Meteoro de Pegasus", "Relampago de Plasma", "Photon Burst", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(true);
+                add(false);
+                add(true);
+                add(false);
+            }
+        };
+        perguntas.add(new Pergunta("(Caveleiros do Zodiaco) Na Saga do Inferno quais são os deuses do sono e da morte que auxiliam Hades?", "Thanatos", "Morpheus", "Hypnos", "Lucifer", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(false);
+                add(true);
+                add(false);
+            }
+        };
+        perguntas.add(new Pergunta("(Sword Art Online) Qual o nome da principal espada usada por Kirito?", "Excalibur", "Hasagi", "Elucidator", "Dark Repulser", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(true);
+                add(false);
+                add(false);
+                add(false);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros Do Zodiaco) Qual o Cavaleiro de Ouro que recebeu a benção do Misopotâmenos (Benção dada pelos deuses e retarda o envelhecimento)?", "Dohko de Libra", "Shura de Capricornio", "Aldebaran de Touro", "Shaka de Virgem", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(true);
+                add(false);
+                add(false);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros Do Zodiaco) Qual o Cavaleiro de Ouro que representa 'O pacifico carneiro que cavalga entre as estrelas' e possui poderes psiquicos?", "Aioros de Sagitario", "Mú de Aries", "Saga de Gêmeos", "Manigold de Cancer", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(true);
+                add(true);
+                add(false);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros Do Zodiaco) Quais dessses golpes pertencem a Seiya de Pegasus?", "Corrente de Andromeda", "Meteoro de Pégaso", "Turbilhão de Pégaso", "Execução Aurora", valores));
+        valores = new ArrayList<Boolean>() {
+            {
+                add(false);
+                add(true);
+                add(false);
+                add(true);
+            }
+        };
+        perguntas.add(new Pergunta("(Cavaleiros Do Zodiaco) Quantas armaduras do Santuario (Bronze, Prata e Ouro) provem de constelações oficiais no anime e no mangá??", "17", "88", "99", "124", valores));
 
     }
 

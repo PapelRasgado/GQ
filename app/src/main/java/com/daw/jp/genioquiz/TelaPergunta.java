27e428a30812a4ad41cd86058af6b04ba50a4ecd
PapelRasgado <PapelRasgado@users.noreply.github.com>
2018-05-03 20:06:38 -0300

    Final 2.0

--- app/src/main/java/com/daw/jp/genioquiz/TelaPergunta.java
+++ app/src/main/java/com/daw/jp/genioquiz/TelaPergunta.java
@@ -2,6 +2,7 @@ package com.daw.jp.genioquiz;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
